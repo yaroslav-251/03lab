c291628c406364f2e987032f7756dd753b392723
yaroslav-251 <kolpakov1803@mail.ru>
2026-09-11 09:00:55 +0300

    Lab03: compoung assignment operators

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Банковский счет");
+
+double balance = 1000;
+Console.WriteLine($"Начальный баланс {balance}");
+
+balance += 500; //пополнение
+Console.WriteLine($"После пополнения на 500: {balance}");
+
+balance -= 200;//покупка на 200
+Console.WriteLine($"После покупки на 200: {balance}");
+
+balance *= 1.05;//начисление 5 процентов
+Console.WriteLine($"После начисления 5%: {balance}");
+
+balance /= 2;//разделили счет напополам с партнером
+Console.WriteLine($"После деления пополам: {balance}");
+
